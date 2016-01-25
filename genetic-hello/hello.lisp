74bc289d7f9048c699b2b1e4079373cdd339fb36
Lucca Fraser <lucca.fraser@gmail.com>
2016-01-25 08:27:18 -0400

    happy birthday, neha

--- genetic-hello/hello.lisp
+++ genetic-hello/hello.lisp
@@ -4,7 +4,8 @@
 (in-package :genetic.hello)
 
 ;(defparameter *target* "Genetic Algorithm")
-(defparameter *target* "You do look, my son, in a moved sort, As if you were dismay'd: be cheerful, sir. Our revels now are ended. These our actors, As I foretold you, were all spirits and Are melted into air, into thin air: And, like the baseless fabric of this vision, The cloud-capp'd towers, the gorgeous palaces, The solemn temples, the great globe itself, Ye all which it inherit, shall dissolve And, like this insubstantial pageant faded, Leave not a rack behind. We are such stuff As dreams are made on, and our little life Is rounded with a sleep. Sir, I am vex'd; Bear with my weakness; my, brain is troubled: Be not disturb'd with my infirmity: If you be pleased, retire into my cell And there repose: a turn or two I'll walk, To still my beating mind")
+;;(defparameter *target* "You do look, my son, in a moved sort, As if you were dismay'd: be cheerful, sir. Our revels now are ended. These our actors, As I foretold you, were all spirits and Are melted into air, into thin air: And, like the baseless fabric of this vision, The cloud-capp'd towers, the gorgeous palaces, The solemn temples, the great globe itself, Ye all which it inherit, shall dissolve And, like this insubstantial pageant faded, Leave not a rack behind. We are such stuff As dreams are made on, and our little life Is rounded with a sleep. Sir, I am vex'd; Bear with my weakness; my, brain is troubled: Be not disturb'd with my infirmity: If you be pleased, retire into my cell And there repose: a turn or two I'll walk, To still my beating mind")
+(defparameter *target* "Happy birthday to you! Happy birthday to you! Happy birthday, dear Neha! Happy birthday to you!")
 (defparameter *creature-len* (length *target*))
 (defparameter *pop-size* 5)
 (defparameter *mutation-rate* 90) ; percent chance of mutation
